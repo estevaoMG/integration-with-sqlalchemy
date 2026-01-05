af8ab2df35b165e7d95b01add6ab340bcdb12424
estevaoMG <estevao.gouveia@hotmail.com>
2026-01-05 15:06:52 -0300

    Definindo os Relacionamentos e Método de Representação

--- integrationWithSQL/SQLAlchemyAppication.py
+++ integrationWithSQL/SQLAlchemyAppication.py
@@ -12,11 +12,26 @@ class User(Base):
     name = Column(String)
     fullname = Column(String)
 
+    address = relationship(
+        "Address", back_populates="user", cascade="all, delete-orphan"
+    )
 
-address = relationship("Address", back_populates="user", cascade="all, delete-orphan")
+    def __repr__(self):
+        return f"User(id={self.id}, name={self.name}, fullname={self.fullname})"
 
 
 class Address(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     email_address = Column(String(30), nullable=False)
     user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)
+
+    user = relationship(
+        "User",
+        back_populates="address",
+    )
+
+    def __repr__(self):
+        return f"Address(id={self.id}, email={self.email_address})"
+
+
+print(User.__tablename__)
